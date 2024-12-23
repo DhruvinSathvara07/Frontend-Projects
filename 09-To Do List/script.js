2f4f6b6790eb204946247deed7bf61110d8258af
DhruvinSathvara07 <sathvaradhruvin05@gmail.com>
2024-12-23 14:45:32 +0530

    first-commit

--- 09-To Do List/script.js
+++ 09-To Do List/script.js
@@ -1,25 +1,76 @@
-const form = document.querySelector(".form");
-const input = document.querySelector("input");
-const button = document.querySelector("button");
-const ul = document.querySelector("ul");
+const form = document.querySelector("form");
+const body = document.querySelector(".tbody");
+const input = form.childNodes[3]
+const textarea = form.childNodes[7]
 
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const data = input.value;
-    const li = document.createElement("li");
-    li.innerHTML =
-        `<span>${data}</span><button class='remove'>Remove</button>`;
-    ul.append(li);
-    input.value = "";
-});
-
-const ulcontainer = document.querySelector(".ulcontainer");
-ulcontainer.addEventListener("click", function (e) {
-    if (e.target.classList.contains("remove")) {
-        const uldata = e.target.parentNode;
-        uldata.remove();
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = {
+        name: input.value,
+        text: textarea.value
     }
+
+    const users = JSON.parse(localStorage.getItem("data")) || []
+    users.push(data)
+    localStorage.setItem("data", JSON.stringify(users))
+    show()
 });
 
+function show() {
+    var users = JSON.parse(localStorage.getItem("data"))
+    var tbody = document.querySelector("#tbody");
+    var result = ""
+
+    users.forEach((items, index) => {
+        result += `
+        <tr class='table-row'>
+            <td>${items.name}</td>
+            <td>${items.text}</td>
+            <td>
+                <button class="btn btn-primary" onclick='handleEdit(${index})'>Edit</button>
+                <button class="btn btn-danger delete" onclick='remove(${index})'>Delete</button>
+            </td>
+        </tr>`;
+    });
+    console.log(users, "this is users");
+    tbody.innerHTML = result
+}
+show()
+
+function handleEdit(id) {
+    const detils = JSON.parse(localStorage.getItem("data"))
+    const update = document.querySelector(".update")
+    update.style.display = "block"
+    const submit = document.querySelector(".submit")
+    submit.style.display = "none"
+
+    const todo = {
+        name: input.value,
+        text: textarea.value
+    };
+
+    input.value = detils[id].name
+    textarea.value = detils[id].text
+
+    update.addEventListener("click", function (e) {
+        e.preventDefault();
+        const data = {
+            name: input.value,
+            text: textarea.value
+        };
+
+        const datalist = JSON.parse(localStorage.getItem("data"))
+        datalist.splice(id, 1, data)
+        localStorage.setItem("data", JSON.stringify(datalist))
+        show()
+        location.reload()
+    })
+}
 
 
+function remove(id) {
+    const data = JSON.parse(localStorage.getItem("data")) || [];
+    data.splice(id, 1);
+    localStorage.setItem("data", JSON.stringify(data));
+    show();
+}
